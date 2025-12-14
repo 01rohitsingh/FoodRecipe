75f3d7733129ac3cd9f9b23aa9c4326d89d9753a
01rohitsingh <rohitgidha2000@gmail.com>
2025-12-14 13:25:40 +0530

    dat-02

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express=require ("express")
 const app =express();
 const dotenv=require("dotenv").config()
-
+const cors=require("cors")
 const connectDb=require("./config/connectionDb")
 
 const PORT=process.env.PORT || 3000
 
 app.use(express.json())
-
+app.use(cors())
 
 app.use("/recipe",require("./routes/recipe"))
 
